953aa50800b13ce1dda823a334afe05b7766a6c6
fazelginanda <fzginanda@gmail.com>
2023-05-07 10:46:28 +0700

    update riwayat

--- src/main/java/tubes2/RiwayatPanel.java
+++ src/main/java/tubes2/RiwayatPanel.java
@@ -1,13 +1,16 @@
 package tubes2;
 import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RiwayatPanel extends JPanel implements ActionListener {
-    
+    private final JComboBox<Integer> opsiAkun;
+    private final JButton tampilkanRiwayatButton;
+    private List<Customer> listCustomer = new ArrayList<>();
+
     public RiwayatPanel() {
         // Bagian Atas
         // Judul
@@ -23,11 +26,78 @@ public class RiwayatPanel extends JPanel implements ActionListener {
         topPanel.setLayout(null);
         topPanel.add(title);
 
+        // Bagian Pilih Akun
+        // Label Pilih Akun
+        JLabel pilihAkunLabel = new JLabel("Pilih Akun");
+        pilihAkunLabel.setForeground(Color.BLACK);
+        pilihAkunLabel.setFont(new Font(pilihAkunLabel.getFont().getName(), pilihAkunLabel.getFont().getStyle(), 15));
+        pilihAkunLabel.setPreferredSize(new Dimension(300,20));
+        // Label ID
+        JLabel idLabel = new JLabel("ID ");
+        idLabel.setForeground(Color.BLACK);
+        idLabel.setOpaque(false);
+        idLabel.setFont(new Font(pilihAkunLabel.getFont().getName(), pilihAkunLabel.getFont().getStyle(), 15));
+        // Opsi Akun
+        /* ----------------------- DUMMY ----------------------- */
+        UnregisteredCustomer a = new UnregisteredCustomer();
+        UnregisteredCustomer b = new UnregisteredCustomer();
+        a.pesan(new FixedBill(), 10);
+        b.pesan(new FixedBill(), 10);
+        listCustomer.add(a);
+        listCustomer.add(b);
+        /* ------------------------------------------------------ */
+        Integer[] opsi = {a.getId(), b.getId()};
+        opsiAkun = new JComboBox<>(opsi);
+        opsiAkun.addActionListener(this);
+        opsiAkun.setFocusable(false);
+        opsiAkun.addActionListener(this);
+        // Sub-Panel Akun
+        JPanel subPanelAkun = new JPanel();
+        subPanelAkun.setPreferredSize(new Dimension(300,30));
+        subPanelAkun.setOpaque(false);
+        subPanelAkun.setLayout(new BorderLayout(10,0));
+        subPanelAkun.add(idLabel, BorderLayout.WEST);
+        subPanelAkun.add(opsiAkun, BorderLayout.CENTER);
+        // Panel Akun
+        JPanel panelAkun = new JPanel();
+        panelAkun.setOpaque(false);
+        panelAkun.setBackground(Color.gray);
+        panelAkun.setLayout(new BorderLayout(0,10));
+        panelAkun.add(pilihAkunLabel, BorderLayout.NORTH);
+        panelAkun.add(subPanelAkun, BorderLayout.SOUTH);
+
+        // Bagian Cari Riwayat
+        // Tombol Cari Riwayat
+        tampilkanRiwayatButton = new JButton("Tampilkan Riwayat");
+        tampilkanRiwayatButton.setFocusable(false);
+        tampilkanRiwayatButton.addActionListener(this);
+        tampilkanRiwayatButton.setPreferredSize(new Dimension(175, 35));
+        // Sub-Panel 1
+        JPanel subPanel1 = new JPanel();
+        subPanel1.setOpaque(false);
+        subPanel1.setPreferredSize(new Dimension(300,30));
+        subPanel1.setLayout(new FlowLayout(FlowLayout.CENTER, 40, 0));
+        subPanel1.add(tampilkanRiwayatButton);
+        // Panel 1
+        JPanel panel1 = new JPanel();
+        panel1.setOpaque(false);
+        panel1.setLayout(new BorderLayout(0,10));
+        panel1.add(subPanel1, BorderLayout.SOUTH);
+
+        // Field Panel
+        JPanel fieldPanel = new JPanel();
+        fieldPanel.setBackground(Color.LIGHT_GRAY);
+        fieldPanel.setPreferredSize(new Dimension(500,300));
+        fieldPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 1000, 45));
+        fieldPanel.add(panelAkun);
+        fieldPanel.add(panel1);
+
         // Center panel
         JPanel centerPanel = new JPanel();
         centerPanel.setPreferredSize(new Dimension(100,100));
         centerPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 1000, 75));
         centerPanel.setBackground(Color.GRAY);
+        centerPanel.add(fieldPanel);
 
         // Main Panel
         setPreferredSize(new Dimension(800,800));
@@ -38,6 +108,6 @@ public class RiwayatPanel extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        //TODO
     }
 }
